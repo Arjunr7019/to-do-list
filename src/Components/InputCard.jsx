9986da1ebc695d32d7fedf9cc80eb1bdfd2c36df
arjun r <arjun.rdell@gmail.com>
2025-01-06 20:50:51 +0530

    add task section added

--- src/Components/InputCard.jsx
+++ src/Components/InputCard.jsx
@@ -5,7 +5,7 @@ export default function InputCard({labelName,inputType}) {
     return (
         <div className='inputCard' style={{ position: "relative",margin:"1rem 0" }}>
             <input style={{ padding: "0.5rem", borderRadius: "1rem", width: "80%" }} type={inputType} id="inputName" />
-            <label style={inputType === "date" ? {display:"none"}:{}} id="inputLabel" htmlFor="inputName">{labelName}</label>
+            <label style={inputType === "date" ? {left:"5%",top:"-33%",backgroundColor:"white"}:{}} id="inputLabel" htmlFor="inputName">{labelName}</label>
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,13 +12,30 @@ export default function Home() {
       </div>
       <div style={{ margin: "0 1rem" }}>
         <h1>Tasks</h1>
+        <div></div>
         <div style={{ display: "flex", flexDirection: "row" }}>
           <div style={{ width: "50%" }}>
-            <InputCard inputType="text" labelName="Task Name"/>
-            <InputCard inputType="text" labelName="Description"/>
-            <InputCard inputType="date" labelName="Date"/>
+            <h3>Add Task</h3>
+            <InputCard inputType="text" labelName="Task Name" />
+            <InputCard inputType="text" labelName="Description" />
+            <InputCard inputType="date" labelName="Dead Line" />
+            <div style={{width:"80%",display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+              <div id='container' style={{width:"50%"}}>
+                <select id='priority' style={{ borderRadius: "1rem", padding: "0.5rem 1rem", textAlign: "center",width:"100%" }}>
+                  <option style={{ textAlign: "start" }} value="high">High</option>
+                  <option style={{ textAlign: "start" }} value="medium">Medium</option>
+                  <option style={{ textAlign: "start" }} value="low">Low</option>
+                </select>
+                <label id='dropdown' htmlFor="container">Priority</label>
+              </div>
+              <div style={{width:"50%",margin:"0 0 0 0.3rem",border:"2px solid gray",borderRadius:"1rem",display:"flex",justifyContent:"center",alignItems:"center"}}>Add</div>
+            </div>
           </div>
+          
+          <div>
+            <h3>Task List</h3>
           <AllList />
+          </div>
         </div>
       </div>
     </div>
