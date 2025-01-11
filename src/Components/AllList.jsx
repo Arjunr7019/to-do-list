48fcbd386fff78bbc020272653a64252038a0028
arjun r <arjun.rdell@gmail.com>
2025-01-11 22:02:09 +0530

    fixed some error and now we are consuming ListContext data

--- src/Components/AllList.jsx
+++ src/Components/AllList.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ToDoListCard from './ToDoListCard'
+import { ListContext } from '../Context/ListContext'
 
 export default function AllList() {
-    const data = [
-        {name:"instagram",date:"01/01/2025",description:"watching reels",priority:"high"},
-        {name:"React JS",date:"01/01/2025",description:"Hooks",priority:"low"},
-        {name:"Watch Movie",date:"01/01/2025",description:"Martin",priority:"high"}
-      ]
+  const { taskList, setTaskLis } = useContext(ListContext);
+
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"1rem" }}>
-          <div style={{ overflowX: "auto", borderRadius: "0.5rem", borderWidth: "1px", borderColor: "#E5E7EB" }}>
-            <table style={{ borderTopWidth: "2px", borderColor: "#E5E7EB", minWidth: "100%", fontSize: "0.875rem", lineHeight: "1.25rem", backgroundColor: "#ffffff" }}>
-              <thead style={{ direction: 'ltr', textAlign: 'left' }}>
-                <tr>
-                  <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Task Name</th>
-                  <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Dead Line</th>
-                  <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Description</th>
-                  <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Priority</th>
-                </tr>
-              </thead>
-    
-              <tbody style={{ borderTopWidth: "1px", borderColor: "#E5E7EB" }}>
-                {data.map((data,index)=> <ToDoListCard name={data.name} date={data.date} description={data.description} priority={data.priority} key={index}/>)}
-              </tbody>
-            </table>
-          </div>
-        </div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "1rem" }}>
+      <div style={{ overflowX: "auto", borderRadius: "0.5rem", borderWidth: "1px", borderColor: "#E5E7EB" }}>
+        <table style={{ borderTopWidth: "2px", borderColor: "#E5E7EB", minWidth: "100%", fontSize: "0.875rem", lineHeight: "1.25rem", backgroundColor: "#ffffff" }}>
+          <thead style={{ direction: 'ltr', textAlign: 'left' }}>
+            <tr>
+              <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Task Name</th>
+              <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Dead Line</th>
+              <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Description</th>
+              <th style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem", fontWeight: 500, color: "#111827", whiteSpace: "nowrap" }}>Priority</th>
+            </tr>
+          </thead>
+
+
+          <tbody style={{ borderTopWidth: "1px", borderColor: "#E5E7EB" }}>
+            {taskList?.map((data, index) => <ToDoListCard name={data.taskName} date={data.date} description={data.description} priority={data.priority} key={index} />)}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
