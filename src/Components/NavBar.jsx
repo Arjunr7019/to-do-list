c98ff3d441162347f488609922b476bc32917696
arjun r <arjun.rdell@gmail.com>
2025-01-14 21:48:25 +0530

    some code fix and improve the code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,8 +4,9 @@ import logo from '../Images/logo.png'
 
 export default function NavBar() {
     return (
-        <div className='navBar'>
-            <img style={{ width: "4%", marginTop: "1rem", marginLeft: "1rem" }} src={logo} alt="logo" />
+        <div className='navBar' style={{display:"flex",flexDirection:"row",alignItems:"center",padding:"1rem"}}>
+            <img style={{ width: "4%" }} src={logo} alt="logo" />
+            <h2 style={{margin:"0"}}>To Do List</h2>
         </div>
     )
 }
